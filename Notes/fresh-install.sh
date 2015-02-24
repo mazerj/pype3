a034b2ab71f10879898080b13f797bf51ebf22d5
James Mazer <james.mazer@yale.edu>
2015-02-24 16:31:52 -0500

    updated instructions/sh

--- Notes/fresh-install.sh
+++ Notes/fresh-install.sh
@@ -1,23 +1,15 @@
-
-# need subversion to pull down snapshot -- since you're
-# looking at this file, it's probably already done..
-
-##sudo apt-get install subversion
-##svn checkout http://pype3.googlecode.com/svn/trunk/ pype3
-
-# install required packages
+# install required packages (this may break for debian because of msfonts)
 sudo sh dep-packages.sh
 
-# libezV24 is for iscan serial port comm -- for some reason it
-# was dropped from Ubuntu 12.04, but picked up in later versions
-# again.. It compiles out of the box, so it's no big deal..
+# need libez for serial port access
+sudo sh ./ez-install.sh
 
-tar xfz libezV24-0.1.3.tar.gz && \
-    cd libezV24-0.1.3 && make && sudo make install && \
-    cd .. && /bin/rm -rf libezV24-0.1.3
-cd ..
+# need eyelink libraries/header files -- install from repository
+sudo sh ./eyelink-repo.sh
 
-# install to /usr/local
 
-sudo ./localinstall build install docs
+cat <<EOF
 
+Now run 'cd ..; ./localinstall' to build and install.
+
+EOF

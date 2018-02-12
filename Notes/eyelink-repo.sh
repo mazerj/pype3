088a0e934a8f6af960cddfa9e7d8f892e0a0b3e5
James Mazer <mazerj@gmail.com>
2018-02-12 09:48:44 -0700

    updated to use latest version of SR repositories

--- Notes/eyelink-repo.sh
+++ Notes/eyelink-repo.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-cat <<EOF >>/etc/apt/sources.list
+if [ `whoami` != root ]; then
+    echo "run as root!"
+    exit 1
+fi
 
-# Eyelink repository (64bit)
-deb http://download.sr-support.com/x64 /
+# run as root..
 
+cat <<EOF >/etc/apt/sources.list.d/eyelink.list
+# Eyelink repository
+deb http://download.sr-support.com/software SRResearch main
 EOF
 
+cd /tmp
+wget http://download.sr-support.com/software/dists/SRResearch/SRResearch_key
+apt-key add SRResearch_key
+rm SRResearch_key
+
 apt-get update
-apt-get install eyelinkcore-1.9 eyelinkcoregraphics-1.9
+apt-get install eyelinkcore eyelinkcoregraphics
 apt-get install -f

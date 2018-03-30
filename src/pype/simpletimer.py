08bb30d5ebd6a9d15f2ad9b40c37381bc5f3fceb
James Mazer <mazerj@gmail.com>
2018-03-30 15:57:17 -0600

    updated to use monotonic.monotonic()

--- src/pype/simpletimer.py
+++ src/pype/simpletimer.py
@@ -14,46 +14,9 @@ for sthis
 
 import os, sys
 
-if sys.platform.startswith('darwin'):
-    import ctypes
-    
-    # src:
-    #   http://twistedmatrix.com/trac/attachment/ticket/2424/linux_and_osx.py
-    libSystem = ctypes.CDLL('libSystem.dylib', use_errno=True)
-    CoreServices = ctypes.CDLL(
-        '/System/Library/Frameworks/CoreServices.framework/CoreServices',
-        use_errno=True)
-    mach_absolute_time = libSystem.mach_absolute_time
-    mach_absolute_time.restype = ctypes.c_uint64
-    AbsoluteToNanoseconds = CoreServices.AbsoluteToNanoseconds
-    AbsoluteToNanoseconds.restype = ctypes.c_uint64
-    AbsoluteToNanoseconds.argtypes = [ctypes.c_uint64]
-    
-    def get_monotonic_ms():
-        return AbsoluteToNanoseconds(mach_absolute_time()) * 1e-6
-elif sys.platform.startswith('linux'):
-    # linux/posix: 
-    import ctypes
-    class timespec(ctypes.Structure):
-        _fields_ = [
-            ('tv_sec', ctypes.c_long),
-            ('tv_nsec', ctypes.c_long)
-        ]
-
-    librt = ctypes.CDLL('librt.so.1', use_errno=True)
-    clock_gettime = librt.clock_gettime
-    clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(timespec)]
-    CLOCK_MONOTONIC = 1 # see <linux/time.h>
-
-    def get_monotonic_ms():
-        ts = timespec()
-        clock_gettime(CLOCK_MONOTONIC, ctypes.pointer(ts))
-        return int(round((ts.tv_sec + (ts.tv_nsec * 1e-9)) * 1000.0))
-else:
-    # fallback to using plain old time.time() and converting from s to ms
-    import time
-    def get_monotonic_ms():
-        return int(round(time.time() * 1000.0))
+import monotonic
+def get_monotonic_ms():
+    return int(round(monotonic.monotonic() * 1000.0))
 
 
 class Timer(object):

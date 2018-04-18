e31aeebfb8c16b56d5cace93cc124015c96d2bf9
James Mazer <mazerj@gmail.com>
2018-04-18 16:24:00 -0600

    - cleaned up comments & code - make not being suid-root a warning instead of an error

--- src/exe/pypebase.c
+++ src/exe/pypebase.c
@@ -1,3 +1,19 @@
+/*
+** This is a thin wrapper for starting up python, possibly
+** with EUID=0. This is because you can't have pure python
+** programs run SUID root.
+**
+** if PYPE is set, then it generates a wrapper that basically
+** exec's:
+**   % python pyperun [comand-line-args]
+** while preserving root access, if possible. If PYPE is not
+** set, then starts up a interpreter by passing `pyperun` the
+** -s (`script`) arg:
+**   % python pyperun -s [command-line-args]
+**
+** pyperun is a pure-python script in ../pype/pyperun
+*/
+
 #include <stdio.h>		/* for stderr() */
 #include <unistd.h>		/* for geteuid() */
 #include <stdlib.h>		/* for exit() */
@@ -9,23 +25,20 @@ int main(int ac, char **av)
 
 #ifdef PYPE
   newac = ac + 2;
-  newav = (char **) calloc(sizeof(char *), (newac + 2));
+  newav = (char **) calloc(sizeof(char *), (newac + 10));
 
   newav[0] = PYTHONEXE;
   newav[1] = RUN;
   for (n = 1; n < (ac+1); n++) {
     newav[1+n] = av[n];
   }
-#ifndef __APPLE__
   if (geteuid() != 0) {
-    fprintf(stderr, "pypeboot: not suid root and not running as root.\n");
-    exit(1);
+    fprintf(stderr, "pypeboot: warning not suid root.\n");
   }
-#endif
   execvp(newav[0], newav);
 #else // PYPENV
   newac = ac + 3;
-  newav = (char **) calloc(sizeof(char *), (newac + 2));
+  newav = (char **) calloc(sizeof(char *), (newac + 10));
 
   newav[0] = PYTHONEXE;
   newav[1] = RUN;

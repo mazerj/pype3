065540bf5f77a3f8d97175557b7dbdcf629b2f78
James Mazer <mazerj@gmail.com>
2019-10-11 11:42:56 -0600

    got rid of imp dependency

--- src/pype/importer.py
+++ src/pype/importer.py
@@ -7,27 +7,33 @@ to print debugging information each time a module gets imported.
 
 """
 
-import builtins
-import sys
-import imp
-from guitools import Logger
+import builtins,sys
+import importlib
+import guitools
 
-_native_imp =  builtins.__import__
+_native_imp = builtins.__import__
 
-def _verbose_import(*args):
+def _verbose_import(*args, **kwargs):
+	"""Really simple hook for reporting imports.
+	"""
+	guitools.Logger('importing %s\n' % args[0])
+	return _native_imp(*args, **kwargs)	
+
+def buggy_verbose_import(*args, **kwargs):
 	try:
-		fp, pathname, description = imp.find_module(args[0])
-		if fp is not None:
-			fp.close()
+		spec = importlib.util.find_spec(args[0])
+		if spec and spec.origin and 'python' not in spec.origin:
 			# only report non-python stdlib imports (this isn't
-            # really the right way to do it, but is reasonably close)
-			if 'python' in pathname:
-				Logger("importing '%s' from '%s'\n" % (args[0], pathname))
-	except:
+			# really the right way to do it, but is reasonably close)
+			 guitools.Logger("importing '%s' from '%s'\n" % \
+							 (args[0], spec.origin))
+	except NameError:
+		guitools.Logger("importing '%s' from '??'\n" % \
+						(args[0], ))
 		# anything goes wrong, punt on error message and fall back
 		# to native import routine..
 		pass
-	return _native_imp(*args)
+	return _native_imp(*args, **kwargs)
 
 def importer(report=1):
 	if report:
